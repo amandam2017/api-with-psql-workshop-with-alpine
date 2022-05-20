f3ec5c9e40d87a6378ffc2d314ddc6ac604d40dc
amandam2017 <amandamaarman19911@gmail.com>
2022-05-20 15:18:25 +0200

    I can now display my garments

--- public/app.js
+++ public/app.js
@@ -1,13 +1,17 @@
 document.addEventListener('alpine:init', () => {
     // alert("!")
     Alpine.data('app', () => ({
+        // garmentOne: '',
         garments : [],
         init() {
-            const self = this;
+            // const self = this;
         	fetch('/api/garments')
         		.then(r => r.json())
         		.then(userData => {
-                    self.garments = userData.data
+                    console.log(userData)
+
+                    this.garments = userData.data
+                    console.log(this.garments)
                 })
         },
         
