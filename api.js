7919d94f5751834da24f243f85db9a96acf26292
amandam2017 <amandamaarman19911@gmail.com>
2022-05-23 20:13:14 +0200

    last changes before I push to heroku...my price filter is still not working properly

--- api.js
+++ api.js
@@ -10,14 +10,14 @@ module.exports = function (app, db) {
 
 		const { gender, season } = req.query;
 		let garments = await db.many('select * from garment');
-		if(season) {
-			garments = await db.many('select * from garment where season = $1',[season]);
+		if (season) {
+			garments = await db.many('select * from garment where season = $1', [season]);
 		}
-		if(gender){
-			garments = await db.manyOrNone('select * from garment where gender = $1',[gender])
+		if (gender) {
+			garments = await db.manyOrNone('select * from garment where gender = $1', [gender])
 		}
-		if(season && gender){
-			garments = await db.many('select * from garment where season = $1 AND gender = $2',[season, gender])
+		if (season && gender) {
+			garments = await db.many('select * from garment where season = $1 AND gender = $2', [season, gender])
 		}
 
 		// if(season && gender === 'Unisex'){
@@ -28,23 +28,36 @@ module.exports = function (app, db) {
 			data: garments
 		})
 	});
-	
-	app.put('/api/garment/:id', async function (req, res) {
-		
+
+	app.get('/api/garments/price/:price', async function (req, res) {
 		try {
-			
+			const { price } = req.params;
+			let garments = await db.many(`select * from garment where price <= $1`, [price]);
+			console.log(garments.length);
+			res.json({
+				data: garments
+			})
+		} catch (error) {
+			console.log(error);
+		}
+	})
+
+	app.put('/api/garment/:id', async function (req, res) {
+
+		try {
+
 			// use an update query...
-			
+
 			const { id } = req.params;
 			const garment = await db.oneOrNone(`select * from garment where id = $1`, [id]);
-			
+
 			// you could use code like this if you want to update on any column in the table
 			// and allow users to only specify the fields to update
-			
+
 			let params = { ...garment, ...req.body };
 			const { description, price, img, season, gender } = params;
-			
-			await db.none(`update garment set gender = $1, description = $2, price = $3, img = $4, season = $5 where id=$6`, [gender,description,price,img,season, id])
+
+			await db.none(`update garment set gender = $1, description = $2, price = $3, img = $4, season = $5 where id=$6`, [gender, description, price, img, season, id])
 			await db.none(`delete * from garment where gender = $1`, [gender]);
 
 			res.json({
@@ -88,8 +101,8 @@ module.exports = function (app, db) {
 			const { description, price, img, season, gender } = req.body;
 
 			// insert a new garment in the database
-			 await db.none(`insert into garment( description, price, img, season, gender) values($1,$2,$3,$4,$5) on conflict do nothing`, [ description, price, img, season, gender]);
-		
+			await db.none(`insert into garment( description, price, img, season, gender) values($1,$2,$3,$4,$5) on conflict do nothing`, [description, price, img, season, gender]);
+
 			res.json({
 				status: 'success',
 
@@ -127,7 +140,7 @@ module.exports = function (app, db) {
 			console.log(err);
 			res.json({
 				status: 'success',
-				error : err.stack
+				error: err.stack
 			})
 		}
 	});
--- public/app.js
+++ public/app.js
@@ -4,35 +4,35 @@ document.addEventListener('alpine:init', () => {
         garments: [],
         seasonFilter: '',
         genderFilter: '',
+        maxPrice: 0,
         init() {
             fetch('/api/garments')
                 .then(r => r.json())
                 .then(results => {
-                    console.log(results)
+                    // console.log(results)
                     this.garments = results.data
-                    console.log(this.garments)
-
+                    // console.log(this.garments)
                 })
 
         },
         filterData() {
             console.log(this.genderFilter)
             fetch(`/api/garments?gender=${this.genderFilter}&season=${this.seasonFilter}`)
-            .then(r => r.json())
-            .then(results=>{
-                console.log(results);
-                this.garments = results.data
-                console.log(this.garments);
-            }).catch(err => console.log('No data'))
+                .then(r => r.json())
+                .then(results => this.garments = results.data)
+                .catch(err => console.log('No data'))
+        },
 
-                // .then(function (result) {
-                //     console.log(result.data);
-                //     this.garments = result.data
-
-                // }).catch(err => console.log('No data'))
+        priceFilter() {
+            console.log(this.maxPrice);
+            fetch(`/api/garments/price/${this.maxPrice}`)
+                .then(r => r.json())
+                .then(function (myresults) {
+                    this.garments = myresults.data
+                    console.log(this.garments);
+                })
+                .catch(err => console.log(err))
         }
-
-
 
     }));
 })
